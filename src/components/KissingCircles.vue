66ac09a7b56e68adb160e6c685348fe0b7fb55d7
Cory Washington <tbd>
2024-06-29 16:14:44 -0400

    clarify (and tweak) circle generation logic and variable/method names

--- src/components/KissingCircles.vue
+++ src/components/KissingCircles.vue
@@ -56,14 +56,11 @@ import type { Ref } from 'vue'
 import BaseCanvas from './BaseCanvas.vue';
 import { Coor } from './../models/coor'
 
-const numCirclesRef: Ref<number> = ref(5)
+const numCirclesRef: Ref<number> = ref(60)
 const animationDurationRef: Ref<number> = ref(10) // seconds
 
-const srcCentersRef: Ref<Coor[]> = ref([])
-const srcCentersOnCircles: Ref<CoorOnACircle[]> = ref([])
-const currCentersOnCircles: Ref<CoorOnACircle[]> = ref([])
-// const dstCentersRef: Ref<Coor[]> = ref([])
-// const currCentersRef: Ref<Coor[]> = ref([])
+const movingCoorsOnCircles: Ref<MovingCoorOnACircle[]> = ref([])
+const routeCircles: Ref<Circle[]> = ref([])
 const animating: Ref<boolean> = ref(false)
 const stopAnimationFlag: Ref<boolean> = ref(false)
 let startTimestamp: number
@@ -76,22 +73,29 @@ let height: number
 let width: number
 let canvasCenter: Coor
 
+// Circle style props
+let colorHueOffset: number = 0
+const colorHueOffsetStepsize: number = 0.3
+
 watch(numCirclesRef, (newNumCircles: number, oldNumCircles: number) => {
   stepAtLeastOnce.value = true
   const numAdditionalCircles: number = newNumCircles - oldNumCircles
   if (numAdditionalCircles < 0) {
     if (initialized) {
-      srcCentersRef.value = srcCentersRef.value.slice(0, newNumCircles)
-      srcCentersOnCircles.value = generateCoorOnCircles(srcCentersRef.value)
+      routeCircles.value = routeCircles.value.slice(0, newNumCircles)
+      movingCoorsOnCircles.value = generateMovingCoorsOnCircles(routeCircles.value)
     }
   } else {
     if (initialized) {
-      const additionalCircles: Coor[] = generateRandomCenters(numAdditionalCircles, srcCentersRef.value)
-      srcCentersRef.value = srcCentersRef.value.concat(additionalCircles)
-      srcCentersOnCircles.value = generateCoorOnCircles(srcCentersRef.value)
+      const additionalRouteCircles: Circle[] = generateRouteCircles(numAdditionalCircles, routeCircles.value)
+      routeCircles.value = routeCircles.value.concat(additionalRouteCircles)
+      movingCoorsOnCircles.value = generateMovingCoorsOnCircles(routeCircles.value)
     }
   }
+})
 
+watch(animationDurationRef, (newAnimationDuration: number) => {
+  stepAtLeastOnce.value = true
 })
 
 function incrementNumCircles() {
@@ -102,10 +106,6 @@ function decrementNumCircles() {
   numCirclesRef.value--
 }
 
-watch(animationDurationRef, (newAnimationDuration: number) => {
-  stepAtLeastOnce.value = true
-})
-
 function incrementAnimationDuration() {
   animationDurationRef.value++
 }
@@ -114,39 +114,30 @@ function decrementAnimationDuration() {
   animationDurationRef.value--
 }
 
-// Circle style props
-let colorHueOffset: number = 0
-const colorHueOffsetStepsize: number = 0.3
+function regenerateCircles() {
+  resetCanvasWithNewCircles()
+}
 
-class CoorOnACircle extends Coor {
-  /** Coordinate of the point on the circle */
-  coor: Coor
+class MovingCoorOnACircle {
   /** Center of the circle the coor is on */
-  center: Coor
-  /** radius of the circle the Coor is on */
-  radius: number
+  routeCircle: Circle
   /** Radian position of Coor on the circle */
-  theta: number
+  initialTheta: number
   /** +1 for clockwise (default), -1 for counter-clockwise, zero for no movement */
   direction: number
   /** Speed of animation movement for this point, must be non-negative.
    * Default is `1.0` for normal speed */
   speed: number
 
-  public constructor(coorOnCircle: Coor, radius: number, theta: number, direction: number = 1, speed: number = 1.0) {
-    const centerOfCircle = new Coor(
-      coorOnCircle.x - (radius * Math.cos(theta)),
-      coorOnCircle.y - (radius * Math.sin(theta))
-    )
-    super(centerOfCircle.x, centerOfCircle.y)
-    this.coor = coorOnCircle
-    this.center = centerOfCircle
-    this.radius = radius
-    this.theta = theta
+  public constructor(routeCircle: Circle, initialTheta: number, direction: number = 1, speed: number = 1.0) {
+    this.routeCircle = routeCircle
+    this.initialTheta = initialTheta
+
     if (![-1, 0, 1].includes(direction)) {
       throw Error(`Direction must be -1, 0, or 1, but was ${direction}`)
     }
     this.direction = direction
+
     if (speed < 0) {
       throw Error(`Speed must be non-negative, but was ${speed}`)
     }
@@ -154,10 +145,9 @@ class CoorOnACircle extends Coor {
   }
 
   public copy() {
-    return new CoorOnACircle(
-      this.coor,
-      this.radius,
-      this.theta,
+    return new MovingCoorOnACircle(
+      this.routeCircle,
+      this.initialTheta,
       this.direction,
       this.speed,
     )
@@ -240,54 +230,48 @@ function initCanvas(ctx: CanvasRenderingContext2D, timestamp: number) {
 }
 
 function resetCanvasWithNewCircles() {
-  srcCentersRef.value = generateRandomCenters(numCirclesRef.value)
-  srcCentersOnCircles.value = generateCoorOnCircles(srcCentersRef.value)
+  routeCircles.value = generateRouteCircles(numCirclesRef.value)
+  movingCoorsOnCircles.value = generateMovingCoorsOnCircles(routeCircles.value)
 }
 
-function generateRandomCenters(n: number, existingCenters: Coor[] = []): Coor[] {
+function generateRouteCircles(n: number, existingRouteCircles: Circle[] = []): Circle[] {
+  const m = existingRouteCircles.length
+  const xStep: number = width * (1/50)
+  const yStep: number = height * (1/50)
+  const thetaStep = (2 * Math.PI) / 6
 
-  const centers: Coor[] = []
-  if (n <= 0) { return centers };
-
-  const m = existingCenters.length
-  const stepSize = 1.0 / 6
-  const xStep: number = (width / 5) * stepSize
-  const yStep: number = (height / 5) * stepSize
-  const xOffset: number = xStep * 10
-  const yOffset: number = yStep * 10
+  const circles: Circle[] = []
   for (let i=0; i<n; i++) {
-    const j=m+i
-    const x: number = (xStep * (j + 1)) + xOffset
-    const y: number = (yStep * (j + 1)) + yOffset
-    const rotation_angle = 2*Math.PI * (j / 5)
-    const cos_theta = Math.cos(rotation_angle)
-    const sin_theta = Math.sin(rotation_angle)
-    const x_rotated = canvasCenter.x + (cos_theta*x - sin_theta*y)
-    const y_rotated = canvasCenter.y + (cos_theta*y + sin_theta*y)
-    centers.push(new Coor(x_rotated, y_rotated))
+    const j=m+i+1
+    const theta = j * thetaStep
+    const xBeforeRotation = canvasCenter.x + (j * xStep)
+    const yBeforeRotation = canvasCenter.y + (j * yStep)
+    const xAfterRotation = (Math.cos(theta) * xBeforeRotation) - (Math.sin(theta) * yBeforeRotation)
+    const yAfterRotation = (Math.sin(theta) * xBeforeRotation) + (Math.cos(theta) * yBeforeRotation)
+    const x = canvasCenter.x + xAfterRotation
+    const y = canvasCenter.y + yAfterRotation
+    const center = new Coor(x, y)
+    const radius = dist(center, canvasCenter)
+
+    circles.push(new Circle(x, y, radius))
   }
-  return centers;
+
+  return circles
 }
 
-function generateCoorOnCircles(centers: Coor[]): CoorOnACircle[] {
-  return centers.map((center, index) => {
-    // const radius = 10 + (190 * Math.random())
-    // const theta = (2 * Math.PI) * Math.random()
-    // const SPEEDS = [1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9]
-    // const SPEEDS = [1.0005, 1.0010, 1.0015, 1.0020, 1.0025, 1.0030, 1.0035, 1.0040]
-    const SPEEDS = [1.0005, 1.0010]
-
-    const diffX = center.x - canvasCenter.x
-    const diffY = center.y - canvasCenter.y
-    const radius = dist(center, canvasCenter)
-    const theta = Math.atan(diffY / diffX)
-    const direction = 1//[1, -1][index % 2]
-    // const speed = SPEEDS[index % SPEEDS.length]
-    const speed = SPEEDS[index % SPEEDS.length] ** index
-    // const speed = [1.001, 1.001, 1.002, 1.002][index % 4] ** index
-
-    return new CoorOnACircle(center, radius, theta, direction, speed)
+function generateMovingCoorsOnCircles(routeCircles: Circle[]): MovingCoorOnACircle[] {
+  const thetaStep = (2 * Math.PI) / 6
+  const movingCoorsOnCircles = routeCircles.map((routeCircle, index) => {
+    const movingCoorOnACircles = new MovingCoorOnACircle(
+      routeCircle,
+      thetaStep * index,
+      1,
+      1.000
+    )
+    return movingCoorOnACircles
   })
+
+  return movingCoorsOnCircles
 }
 
 function computeRadii(centers: Coor[]): CircleWithRadiusLine[] {
@@ -348,10 +332,11 @@ function renderKissingCircles(centers: Coor[], ctx: CanvasRenderingContext2D) {
     ctx.fillStyle = "hsl(0 0% 0% / 0%)"
     // ctx.strokeText(`(${center.x.toFixed(1)}, ${center.y.toFixed(1)}), ${radius.toFixed(1)}`, center.x-5, center.y)
 
-    // Add line segment pointing to nearest neighbor
+    // // Add line segment pointing to nearest neighbor
     // const radiusLine = circlesWithRadiusLine.radiusLine as LineSegment
     // ctx.moveTo(radiusLine.src.x, radiusLine.src.y);
     // ctx.lineTo(radiusLine.dst.x, radiusLine.dst.y)
+    // ctx.stroke();
 
     // Draw center dot
     const dotRadius = 5
@@ -367,10 +352,10 @@ function renderKissingCircles(centers: Coor[], ctx: CanvasRenderingContext2D) {
   colorHueOffset += colorHueOffsetStepsize
 }
 
-function renderSrcCentersOnCircles(ctx: CanvasRenderingContext2D) {
-  srcCentersOnCircles.value.forEach((srcCenterOnCircle, index) => {
-    const center = srcCenterOnCircle.center
-    const radius = srcCenterOnCircle.radius
+function renderRouteCircles(ctx: CanvasRenderingContext2D) {
+  routeCircles.value.forEach((routeCircle, index) => {
+    const center = routeCircle.center
+    const radius = routeCircle.radius
 
     ctx.beginPath();
     ctx.setLineDash([7,3]);
@@ -378,6 +363,14 @@ function renderSrcCentersOnCircles(ctx: CanvasRenderingContext2D) {
 
     ctx.stroke();
     ctx.setLineDash([]);
+
+    // Draw center dot
+    const dotRadius = 3
+    ctx.beginPath();
+    ctx.arc(center.x, center.y, dotRadius, 0,2*Math.PI);
+    ctx.fillStyle = `hsl(${(index / numCirclesRef.value) * 360 + colorHueOffset} 100% 50% / 100%)`
+    ctx.fill()
+    ctx.fillStyle = "hsl(0 0% 0% / 0%)"
   })
 
   colorHueOffset += colorHueOffsetStepsize
@@ -387,16 +380,7 @@ function dist(a: Coor, b: Coor): number {
   return Math.sqrt((a.x - b.x)**2 + (a.y - b.y)**2)
 }
 
-function regenerateCircles() {
-  resetCanvasWithNewCircles()
-}
-
 function animate() {
-  // dstCentersRef.value = generateRandomCenters()
-
-  // Shuffle srcCenters to produce dstCenters
-  // dstCentersRef.value = (srcCentersRef.value.map(c => [Math.random(), c]) as [number,Coor][]).sort().map(el => el[1])
-
   animating.value = true
   stopAnimationFlag.value = false
   // Identical to `timeStamp` used in `window.requestAnimationFrame`
@@ -406,9 +390,6 @@ function animate() {
 
 function _addShapes(ctx: CanvasRenderingContext2D, timestamp: number) {
   initialized || initCanvas(ctx, timestamp)
-
-  // Don't process stopAnimationFlag till end, to make sure the current
-  // batch of shapes gets painted.
 
   let elapsed: number = timestamp - startTimestamp;
 
@@ -423,47 +404,26 @@ function _addShapes(ctx: CanvasRenderingContext2D, timestamp: number) {
   // Loop animation, instead of stop animation after animationDuration
   stepSize = elapsed / (animationDurationRef.value * 1000)
 
-  let newCenters: Coor[] = []
-  currCentersOnCircles.value = []
+  let kissingCircleCenters: Coor[] = []
   for (let i=0; i<numCirclesRef.value; i++) {
-    // const x = srcCentersRef.value[i].x + (dstCentersRef.value[i].x - srcCentersRef.value[i].x) * stepSize
-    // const y = srcCentersRef.value[i].y + (dstCentersRef.value[i].y - srcCentersRef.value[i].y) * stepSize
-    //
-    // const MAX_PETURB = 1
-    // const xPeturb = MAX_PETURB * Math.random()
-    // const yPeturb = MAX_PETURB * Math.random()
-    // const x = centerOfCircle.x + (radius * Math.cos(theta)) + xPeturb
-    // const y = centerOfCircle.y + (radius * Math.sin(theta)) + yPeturb
+    const movingCoorOnCircle = movingCoorsOnCircles.value[i]
+    const routeCircle = movingCoorOnCircle.routeCircle
+    const radius = routeCircle.radius
+    const thetaOffset = (2 * Math.PI) * (movingCoorOnCircle.direction * movingCoorOnCircle.speed) * stepSize
+    const theta = movingCoorOnCircle.initialTheta + thetaOffset
+    const x = routeCircle.center.x + (radius * Math.cos(theta))
+    const y = routeCircle.center.y + (radius * Math.sin(theta))
 
-    const srcCenterOnCircle = srcCentersOnCircles.value[i]
-    const centerOfCircle = srcCenterOnCircle.center
-    const radius = srcCenterOnCircle.radius
-    const thetaOffset = (2 * Math.PI) * (srcCenterOnCircle.direction * srcCenterOnCircle.speed) * stepSize
-    const theta = srcCenterOnCircle.theta + thetaOffset
-    const x = centerOfCircle.x + (radius * Math.cos(theta))
-    const y = centerOfCircle.y + (radius * Math.sin(theta))
-
-    const newCenter: Coor = new Coor(x, y)
-    newCenters.push(newCenter)
-
-    const newCoorOnCircle: CoorOnACircle = new CoorOnACircle(
-      newCenter,
-      radius,
-      theta,
-      srcCenterOnCircle.direction,
-      srcCenterOnCircle.speed
-    )
-    currCentersOnCircles.value.push(newCoorOnCircle)
+    const kissingCircleCenter: Coor = new Coor(x, y)
+    kissingCircleCenters.push(kissingCircleCenter)
   }
 
-  renderKissingCircles(newCenters, ctx)
-  renderSrcCentersOnCircles(ctx)
+  renderKissingCircles(kissingCircleCenters, ctx)
+  // renderRouteCircles(ctx)
 
   if (stopAnimationFlag.value) {
     stopAnimationFlag.value = false
     animating.value = false
-    // srcCentersRef.value = currCentersRef.value
-    // srcCentersOnCircles.value = currCentersOnCircles.value
   }
 
   return
