4b39f9f2e5a3f9de79e6583bb25024e43222afc1
Cory Washington <tbd>
2024-07-27 20:16:32 -0400

    Some more Coor tests

--- src/models/coor.spec.ts
+++ src/models/coor.spec.ts
@@ -6,8 +6,8 @@ describe('Coor', () => {
   it('initializes correctly', () => {
     const a = new Coor(5, 2.67)
 
-    expect(a.x).toEqual(5)
-    expect(a.y).toEqual(2.67)
+    expect.soft(a.x).toEqual(5)
+    expect.soft(a.y).toEqual(2.67)
   })
 
   it('add correctly', () => {
@@ -15,8 +15,8 @@ describe('Coor', () => {
     const b = new Coor(-3, 8.2)
     const c = new Coor(-2, 17.2)
 
-    expect(a.add(b)).toEqual(c)
-    expect(b.add(a)).toEqual(c)
+    expect.soft(a.add(b)).toEqual(c)
+    expect.soft(b.add(a)).toEqual(c)
   })
 
   it('subtracts correctly', () => {
@@ -24,23 +24,28 @@ describe('Coor', () => {
     const b = new Coor(-3, 8.2)
     const c = new Coor(-2, 17.2)
 
-    expect(c.subtract(b)).toEqual(a)
-    expect(c.subtract(a)).toEqual(b)
+    expect.soft(c.subtract(b)).toEqual(a)
+    expect.soft(c.subtract(a)).toEqual(b)
   })
 
   it('rotates correctly', () => {
     const a = new Coor(1, 9)
 
     const distTolerance = 1e-10
-    expect(dist(a.rotate(Math.PI*0.0), a)).toBeLessThan(distTolerance)
-    expect(dist(a.rotate(Math.PI*0.5), new Coor(-9, 1))).toBeLessThan(distTolerance)
-    expect(dist(a.rotate(Math.PI*1.0), new Coor(-1, -9))).toBeLessThan(distTolerance)
-    expect(dist(a.rotate(Math.PI*1.5), new Coor(9, -1))).toBeLessThan(distTolerance)
-    expect(dist(a.rotate(Math.PI*0.25), new Coor(-4*Math.sqrt(2), 5*Math.sqrt(2)))).toBeLessThan(distTolerance)
-    expect(dist(a.rotate(Math.PI*0.75), new Coor(-5*Math.sqrt(2), -4*Math.sqrt(2)))).toBeLessThan(distTolerance)
+    expect.soft(dist(a.rotate(Math.PI*0.0), a)).toBeLessThan(distTolerance)
+    expect.soft(dist(a.rotate(Math.PI*0.5), new Coor(-9, 1))).toBeLessThan(distTolerance)
+    expect.soft(dist(a.rotate(Math.PI*1.0), new Coor(-1, -9))).toBeLessThan(distTolerance)
+    expect.soft(dist(a.rotate(Math.PI*1.5), new Coor(9, -1))).toBeLessThan(distTolerance)
+    expect.soft(dist(a.rotate(Math.PI*0.25), new Coor(-4*Math.sqrt(2), 5*Math.sqrt(2)))).toBeLessThan(distTolerance)
+    expect.soft(dist(a.rotate(Math.PI*0.75), new Coor(-5*Math.sqrt(2), -4*Math.sqrt(2)))).toBeLessThan(distTolerance)
   })
 
-  it('add correctly', () => {
+  it('scales correctly', () => {
     const a = new Coor(1, 9)
+
+    expect.soft(a.scale(1)).toEqual(a)
+    expect.soft(a.scale(-1)).toEqual(new Coor(-1, -9))
+    expect.soft(a.scale(1.5)).toEqual(new Coor(1.5, 13.5))
+    expect.soft(a.scale(0)).toEqual(new Coor(0, 0))
   })
 })
